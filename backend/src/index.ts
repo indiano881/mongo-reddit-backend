38e6c2423427ab34ebd1a4b522bc9de7fbaf98cc
davidebaldi <d.baldi88@gmail.com>
2024-12-02 15:31:19 +0100

    hotfix auth router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,12 @@ import mongoose from "mongoose"
 import 'dotenv/config'
 
 import { postRouter } from "./routes/post";
+import { authRouter } from "./routes/auth";
 
 const app= express();
-
+app.use(express.json())//becuase not inbuild in express
 app.use(postRouter)
-
+app.use(authRouter)
 mongoose.connect(process.env.DB_URL!).then(()=> {
     const port= process.env.PORT || "8080";
     app.listen(port, ()=> {
