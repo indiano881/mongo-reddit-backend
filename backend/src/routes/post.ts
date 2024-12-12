c943e947cb0d5a71ceba8ef7784ec251e94be82a
davidebaldi <d.baldi88@gmail.com>
2024-12-12 09:45:13 +0100

    createPost + posts routes done and tested in postman

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,4 +1,6 @@
 import { Router, type Request, type Response } from "express";
+import { Post } from "../models/posts";
+import { authenticate } from "../middlewares/authenticate";
 
 const getPosts= async ( req: Request, res: Response) => {
 
@@ -14,7 +16,32 @@ const getSinglePost= async (req: Request, res: Response)=> {
     console.log(req)
 }
 
+const createPost= async(req: Request , res:Response )=> {
+    try {
+        const {title, content} = req.body;
+        if (!title || typeof title !== 'string') {
+            res.status(400).json({message: 'not valid tipe of input'})
+            return
+        }
+        if (content !== undefined && typeof content !== 'string') {
+            res.status(400).json({message: 'not valid tipe of input'})
+            return
+        }
+
+        const post= await Post.create({
+            title,
+            content,
+            author: req.userId,
+        })
+
+        res.status(201).json({id: post._id})
+    } catch (error) {
+        res.status(500).send()
+    }
+}
+
 export const postRouter= Router();
 
 postRouter.get("/posts", getPosts);
 postRouter.get("/posts/:id", getSinglePost)
+postRouter.post('/posts', authenticate, createPost)
